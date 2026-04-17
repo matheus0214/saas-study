8da5834ac636d01a9413369ca2ae5c4fdf232d74
Matheus G. Dias <matheusgiga123@gmail.com>
2026-04-17 10:20:55 -0300

    feat: implement authentication module with user roles and permissions

---
+++ packages/auth/src/permissions.ts
@@ -0,0 +1,20 @@
+import type { AbilityBuilder } from '@casl/ability'
+
+import type { AppAbility } from '.'
+import type { User } from './models/user'
+
+type PermissionsByRole = (
+  user: User,
+  builder: AbilityBuilder<AppAbility>,
+) => void
+
+type Roles = 'ADMIN' | 'MEMBER'
+
+export const permissions: Record<Roles, PermissionsByRole> = {
+  ADMIN: (_, { can }) => {
+    can('manage', 'all')
+  },
+  MEMBER: (_, { can }) => {
+    can('invite', 'User')
+  },
+}
