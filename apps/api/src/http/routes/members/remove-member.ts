82597b793ff6d6b82c17eac1c8364f4e21fefb5c
Matheus G. Dias <matheusgiga123@gmail.com>
2026-05-07 00:01:44 -0300

    feat: enable to remove a member from the organization
    - User can remove an organizaton member
    - User need to had the correct permissions
    - Only remove member from the same organization

---
+++ apps/api/src/http/routes/members/remove-member.ts
@@ -0,0 +1,71 @@
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+import { prisma } from '@/lib/prisma'
+
+import { getUserPermissions } from '@/utils/get-user-permissions'
+import { UnauthorizedError } from '../_errors/unauthorized-error'
+import { BadRequestError } from '../_errors/bad-request-error'
+
+export async function removeMember(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .delete(
+      '/organizations/:slug/members/:memberId',
+      {
+        schema: {
+          tags: ['Members'],
+          summary: 'Remove a member from the organization',
+          security: [{ bearerAuth: [] }],
+          params: z.object({
+            slug: z.string(),
+            memberId: z.uuid(),
+          }),
+          response: {
+            204: z.null(),
+          },
+        },
+      },
+      async (request, reply) => {
+        const { slug, memberId } = request.params
+        const userId = await request.getCurrentUserId()
+        const { organization, membership } =
+          await request.getUserMembership(slug)
+
+        const { cannot } = getUserPermissions(userId, membership.role)
+
+        if (cannot('update', 'User')) {
+          throw new UnauthorizedError(
+            `You're not allowed to remove this member from the organization.`
+          )
+        }
+
+        const member = await prisma.member.findUnique({
+          where: {
+            id: memberId,
+            organization: {
+              id: organization.id,
+            },
+          },
+        })
+
+        if (!member) {
+          throw new BadRequestError('The provided member not exits.')
+        }
+
+        await prisma.member.delete({
+          where: {
+            id: memberId,
+            organization: {
+              id: organization.id,
+            },
+          },
+        })
+
+        return reply.status(204).send(null)
+      }
+    )
+}
