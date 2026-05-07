558cf0bd5fe2cb3239b0e619746228745fe28f66
Matheus G. Dias <matheusgiga123@gmail.com>
2026-05-06 21:09:08 -0300

    feat: get project by org slug and project slug

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -27,6 +27,7 @@ import { updateOrganization } from './routes/orgs/update-organization'
 import { transferOrganization } from './routes/orgs/transfer-organization'
 import { createProject } from './routes/projects/create-project'
 import { deleteProject } from './routes/projects/delete-project'
+import { getProject } from './routes/projects/get-project'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -81,6 +82,7 @@ app.register(shutdownOrganization)
 app.register(transferOrganization)
 app.register(createProject)
 app.register(deleteProject)
+app.register(getProject)
 
 app.setErrorHandler(errorHandler)
 
