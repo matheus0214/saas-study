303584a59a4b877933165520435552d26c68e752
Matheus G. Dias <matheusgiga123@gmail.com>
2026-05-09 14:19:06 -0300

    feat: add endpoint to reject an invite

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -37,6 +37,7 @@ import { createInvite } from './routes/invites/create-invite'
 import { getInvite } from './routes/invites/get-invite'
 import { getInvites } from './routes/invites/get-invites'
 import { acceptInvite } from './routes/invites/accept-invite'
+import { rejectInvite } from './routes/invites/reject-invite'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -105,6 +106,7 @@ app.register(createInvite)
 app.register(getInvite)
 app.register(getInvites)
 app.register(acceptInvite)
+app.register(rejectInvite)
 
 app.setErrorHandler(errorHandler)
 
