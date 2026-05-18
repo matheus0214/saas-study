5c5428406bc85d92e0dd229818513eab22634bb3
Matheus G. Dias <matheusgiga123@gmail.com>
2026-05-18 13:59:44 -0300

    feat(auth): add isAuthenticated function to verify is user already logged in
    - Prevent user to access login's page

--- apps/web/src/app/auth/layout.tsx
+++ apps/web/src/app/auth/layout.tsx
@@ -1,10 +1,17 @@
+import { redirect } from 'next/navigation'
+
+import { isAuthenticated } from '@/auth/auth'
 import { cn } from '@/lib/utils'
 
-export default function AuthLayout({
+export default async function AuthLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  if (await isAuthenticated()) {
+    redirect('/')
+  }
+
   return (
     <div
       className={`${cn('dark')} flex min-h-screen flex-col items-center justify-center px-4`}
