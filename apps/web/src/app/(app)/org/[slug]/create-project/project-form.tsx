7cc31f94024aa0ab858ddcbea05db8969cdff68e
Matheus G. Dias <matheusgiga123@gmail.com>
2026-06-30 10:11:02 -0300

    feat(web): use interceptor to create project

--- apps/web/src/app/(app)/org/[slug]/create-project/project-form.tsx
+++ apps/web/src/app/(app)/org/[slug]/create-project/project-form.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { AlertTriangle, Loader2 } from 'lucide-react'
+import { useParams } from 'next/navigation'
 
 import { getErrorMessage } from '@/app/auth/utils/get-actions-error'
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
@@ -8,12 +9,21 @@ import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Textarea } from '@/components/ui/textarea'
 import { useFormState } from '@/hooks/use-form-state'
+import { queryClient } from '@/lib/react-query'
 
 import { createProjectAction } from './actions'
 
 export function ProjectForm() {
+  const { slug: org } = useParams<{ slug: string }>()
+
   const [{ success, message, errors }, handleSubmit, isPending] =
     useFormState(createProjectAction)
+
+  if (success) {
+    queryClient.invalidateQueries({
+      queryKey: [org, 'projects'],
+    })
+  }
 
   return (
     <form action="" onSubmit={handleSubmit} className="space-y-4">
