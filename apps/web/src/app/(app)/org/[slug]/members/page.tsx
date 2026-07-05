126d7f896ce6f34ef0c1e9dfcddc732bac3f29f6
Matheus G. Dias <matheusgiga123@gmail.com>
2026-07-04 22:56:09 -0300

    feat(web): initialize members page

---
+++ apps/web/src/app/(app)/org/[slug]/members/page.tsx
@@ -0,0 +1,22 @@
+import { ability } from '@/auth/auth'
+
+import { Invites } from './invites'
+import { MemberList } from './member-list'
+
+export default async function Members() {
+  const permissions = await ability()
+
+  const canListInvites = permissions?.can('get', 'Invite')
+  const canListUsers = permissions?.can('get', 'User')
+
+  return (
+    <div className="space-y-4">
+      <h1 className="text-2xl font-bold">Members</h1>
+
+      <div className="space-y-4">
+        {canListInvites && <Invites />}
+        {canListUsers && <MemberList />}
+      </div>
+    </div>
+  )
+}
---
+++ apps/web/src/http/get-members.ts
@@ -0,0 +1,20 @@
+import { Role } from '@saas/auth'
+
+import { api } from './api-client'
+
+type GetMembersRequest = {
+  members: {
+    id: string
+    userId: string
+    role: Role
+    name: string | null
+    email: string
+    avatarUrl: string | null
+  }[]
+}
+
+export async function getMembers(org: string): Promise<GetMembersRequest> {
+  const result = await api.get<GetMembersRequest>(`/org/${org}/members`).json()
+
+  return result
+}
